10a0fe704591df1f3ad187f6c9c87bc0c553f6b7
Sandro Schwager <sandro.schwager@fhnw.ch>
2020-10-05 15:14:39 +0200

    Added Marker object.

--- RoundPen/RoundPen.cpp
+++ RoundPen/RoundPen.cpp
@@ -1,10 +1,9 @@
-#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
 #include <opencv2/videoio.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 #include <iostream>
 #include <stdio.h>
-#include "cvui.h"
 
 using namespace std;
 using namespace cv;
